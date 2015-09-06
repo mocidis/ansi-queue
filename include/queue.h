b0b3bd2513e0d7faaf568a7540aaf9b979d8028a
Tung HOANG <tung.hoang@gmail.com>
2015-09-06 20:26:35 +0700

    fix queue.h

--- include/queue.h
+++ include/queue.h
@@ -1,13 +1,5 @@
-//
-//  queue_type.h
-//  CallLogicManager
-//
-//  Created by Tung HOANG on 12/22/13.
-//  Copyright (c) 2013 TungMacBook. All rights reserved.
-//
-
-#ifndef __ANSI_CONCURRENT_QUEUE_H_
-#define _ANSI_CONCURRENT_QUEUE_H_
+#ifndef __ANSI_CONCURRENT_QUEUE_H__
+#define __ANSI_CONCURRENT_QUEUE_H__
 #include <pthread.h>
 #include <stdio.h>      /* printf, scanf, NULL */
 #include <stdlib.h>     /* calloc, exit, free */
@@ -15,29 +7,10 @@
 #define QUEUE_CAP 100
 
 #define QE_EXIT 18834
-#if 0
-typedef struct __event {
-  int is_used;
-  int type;
-  void *data;
-  void *p_user;
-} queue_event_t;
-
-/* QUEUE EVENT POOL */
-typedef struct {
-    queue_event_t *p_queue_events;
-    char *__p_qe_data;
-    int capacity;
-    int data_size;
-    int count;
-    pj_mutex_t *p_mutex;
-} qepool_t;
-#endif
 
 typedef struct __queue queue_t;
 struct __queue
 {
-//    qepool_t qepool;
     void **buffer;
     int capacity;
     int size;
@@ -52,8 +25,4 @@ void queue_enqueue(queue_t *queue, void *value);
 void *queue_dequeue(queue_t *queue);
 int queue_size(queue_t *queue);
 void queue_init(queue_t *queue, int cap, int data_size);
-
-//void qepool_init(qepool_t *p_qepool, int capacity, int data_size, pj_pool_t *p_mempool);
-//queue_event_t *qepool_get(qepool_t *p_qepool);
-//void qepool_free(qepool_t *p_qepool, queue_event_t *p_event);
 #endif
